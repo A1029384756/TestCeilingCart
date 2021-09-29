54b0dce798eb0b69c98027cdf6c8b60a381445f4
A1029384756 <hayden.gray104@gmail.com>
2021-09-28 21:35:41 -0400

    Added protothreading

--- MotorCartCode.cpp
+++ MotorCartCode.cpp
@@ -1,9 +1,10 @@
 #include <Stepper.h>
 #include <IRremote.h>
+#include <protothreads.h>
 //Pin Definitions
 #define echoPinFront 0
-#define echoPinRear 2
 #define trigPinFront 1
+#define echoPinRear 2
 #define trigPinRear 3
 
 //Motor Objects
@@ -15,18 +16,16 @@ decode_results results;
 
 //Data Fields & Constants
 int speed;
-int frontDist, rearDist;
+long frontDist, rearDist;
 int borderDist = 6;
-int speedMultiplier = 3;
-
-//Protothreading Fields
-unsigned long previousMicros;
-int maxMotorOffTime = 100;
-int maxRemoteOffTime = 528;
-int maxDistanceOffTime = 748;
+int maxSpeed = 3;
 
 void setup()
 {
+    PT_INIT(&ptFindDistance);
+    PT_INIT(&ptRunMotor);
+    PT_INIT(&ptReadRemote);
+    
     Stepper1.setSpeed(0);
     
     //Set Pin Modes
@@ -45,105 +44,90 @@ void setup()
 
 void loop()
 {
-    unsigned long currentMicros = micros();
-    
-    if (currentMicros - previousMicros >= maxMotorOffTime)
-    {
-        runMotor();
-        previousMicros = currentMicros;
-    }
-    
-    else if (currentMicros - previousMicros >= maxRemoteOffTime)
-    {
-        //Set motor speed with the remote readout
-        speed += speedMultiplier * readRemote();
-        previousMicros = currentMicros;
-    }
-    
-    else if (currentMicros - previousMicros >= maxDistanceOffTime)
-    {
-        findDistance();
-        
-        //Find cart relation to motor borders and set speed speed caps
-        if (frontDist > borderDist && rearDist > borderDist)
-        {
-            speed = setMaxMin(speed, 2 * speedMultiplier, -2 * speedMultiplier);
-        }
-        
-        if (frontDist < borderDist)
-        {
-            speed = setMaxMin(speed, 0, -2 * speedMultiplier);
-        }
-        
-        if (rearDist < borderDist)
-        {
-            speed = setMaxMin(speed, 2 * speedMultiplier, 0);
-        }
-        
-        previousMicros = currentMicros;
-    }
-    
-    else
-    {
-        runMotor();
-    }
+    speed = setMaxMin(speed, 2 * speedMultiplier * (frontDist > 3), -2 * speedMultiplier * (rearDist > 3));
+    PT_SCHEDULE(runMotor(&ptRunMotor));
+    PT_SCHEDULE(readRemote(&ptReadRemote));
+    PT_SCHEDULE(findDistance(&ptFindDistance));
 }
 
-void findDistance()
+pt ptFindDistance;
+int findDistance(struct pt* pt)
 {
-    //Emit Output Pulse
-    digitalWrite(trigPinFront, LOW);
-    digitalWrite(trigPinRear, LOW);
-    delayMicroseconds(2);
-    
-    digitalWrite(trigPinFront, HIGH);
-    digitalWrite(trigPinRear, HIGH);
-    delayMicroseconds(10);
-    
-    digitalWrite(trigPinFront, LOW);
-    digitalWrite(trigPinRear, LOW);
-    
-    //Receive Output Pulse
-    frontDist = int(pulseIn(0, HIGH) * 0.034 / 2);
-    rearDist = int(pulseIn(1, HIGH) * 0.034 / 2);
-}
-
-void runMotor()
-{
-    //Set motor speed and run steppers
-    Stepper1.setSpeed(speed);
-    if (speed != 0)
+    PT_BEGIN(pt);
+    for(;;)
     {
-        Stepper1.step(1);
+        digitalWrite(trigPinFront, LOW);
+        digitalWrite(trigPinRear, LOW);
+        delayMicroseconds(2);
+        
+        digitalWrite(trigPinFront, HIGH);
+        
+        digitalWrite(trigPinRear, HIGH);
+        delayMicroseconds(10);
+        
+        digitalWrite(trigPinFront, LOW);
+        digitalWrite(trigPinRear, LOW);
+        
+        frontDist = pulseIn(echoPinFront, HIGH)/29/2;
+        rearDist = pulseIn(echoPinRear, HIGH)/29/2;
+        
+        PT_YIELD(pt);
     }
+    PT_END(pt);
 }
 
-int readRemote()
+pt ptRunMotor;
+int runMotor(struct pt* pt)
 {
-    int output = 0;
-    //Run decode operation on ir reciever
-    if(irrecv.decode(&results))
+    PT_begin(pt);
+    for(;;)
     {
-        int value = results.value;
-        Serial.println(value);
-        //Return results based on hex code
-        switch(value)
+        //Set motor speed and run steppers
+        Stepper1.setSpeed(speed);
+        if (speed != 0)
         {
-            case 0xFFE01:
-            output--;
-            break;
+            Stepper1.step(1);
+        }
+        PT_YIELD(pt);
+    }
+    
+    PT_END(pt);
+}
+
+pt ptReadRemote;
+int readRemote(struct pt* pt)
+{
+    PT_BEGIN(pt);
+    for(;;)
+    {
+        int output = 0;
+        //Run decode operation on ir reciever
+        if(irrecv.decode(&results))
+        {
+            int value = results.value;
+            Serial.println(value);
+            //Return results based on hex code
+            switch(value)
+            {
+                case 0xFFE01:
+                speed--;
+                break;
+                
+                case 0xFF906:
+                speed++;
+                break;
+                
+                default:
+                break;
+            }
             
-            case 0xFF906:
-            output++;
-            break;
-            
-            default:
-            break;
+            irrecv.resume();
         }
         
-        irrecv.resume();
+        PT_SLEEP(pt, 100);
     }
-    return output;
+    
+    PT_END(pt);
 }
 
 int setMaxMin(int x, int max, int min)
